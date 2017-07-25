1b834acd323906978c36e454349d8c9c90ea56b1
gFleancu <geani.fleancu@tremend.com>
2017-07-25 17:27:50 +0300

    day 2.2

--- src/main/java/Ex/L_2.java
+++ src/main/java/Ex/L_2.java
@@ -23,9 +23,64 @@ public class L_2 {
         variabila cu virgulă mobilă într-o valoare întreagă.
         */
 
-        double var = 15.757;
-        int varConv = (int) var;
+        double varDbl = 15.757;
+        int varConv = (int) varDbl;
         System.out.println("Var converita " + varConv);
+
+
+
+        /*
+        Exercițiul 1
+        Numele variabilelor trebuie schimbate astfel încât să fie corecte
+        sintactic şi în acord cu convenţiile:
+        String 1stUserName;
+        int bdt; //date of birthday
+        float o1; //first operand
+         */
+        String firstUserName;
+        int birthDate;
+        float firstOperand;
+
+        /*
+        Exercițiul 2
+        Editaţi numele constantei:
+        public static final short pageWidth = 800;
+         */
+
+        /*public static*/
+        final short PAGE_WIDTH = 800;
+
+        /*
+        Exerciţiul 1:
+        Unitate: Operatori
+        Este necesar să creăm un program care adună două numere
+        reprezentate prin două variabile. Primul număr este variabila x, cu
+        valoarea 1, iar celălalt este variabila y, cu valoarea 2. Trebuie să
+        atribuim variabilei z rezultatul adunării acestor două variabile.
+         */
+
+        //definire x
+        int x = 1;
+        //definire y
+        int y = 2;
+        //calcul z=x+y
+        int z = x + y;
+        //afisare z
+        System.out.println("Valoare z = " + z);
+
+        /*
+        Exerciţiul 2
+        Trebuie să creăm un program care calculează aria unui cerc.
+         */
+
+        //definire PI
+        final double PI = 3.14;
+        //definire raza
+        double r = 15;
+        //calcul arie PI*r^2
+        double arie = PI * (r * r);
+        //afisare arie
+        System.out.println("Arie  = " + arie);
 
 
 //end
--- src/main/java/JavaTests.java
+++ src/main/java/JavaTests.java
@@ -1,4 +1,24 @@
+import java.util.Arrays;
+
+
 public class JavaTests {
+
+    private int classVariable;
+
+    private static int classVariable2;
+
+    private static int variableA; // variabila de clasă
+
+// variabile de instanţă
+
+    private int variableB;
+
+    private String variableC;
+
+    private boolean variableD;
+
+    private static final double PI = 3.14;
+
 
     public static void main(String[] args) {
 
@@ -6,8 +26,8 @@ public class JavaTests {
        /* System.out.println("Geani test2");
         System.out.println("Geani test3");*/
 
-        Integer x = 81;
-        if (x < 10) {
+        Integer xn = 81;
+        if (xn < 10) {
             System.out.println("\n" + "Ei manacrena");
         } else {
             System.out.println("\n" + "no macarena");
@@ -27,7 +47,7 @@ public class JavaTests {
 
         int binVal = 0b11010; // 2^1 + 2^3 + 2^4 = 2 + 8 + 16 = 26
 
-        System.out.println(x.getClass());
+        System.out.println(xn.getClass());
 
         double z = 10.5;
 
@@ -56,6 +76,146 @@ public class JavaTests {
         byte nybbles = 0b0010_0101;
 
         long bytes = 0b11010010_01101001_10010100_10010010;
+
+        System.out.println("\n");
+
+
+        JavaTests objClass = new JavaTests();
+
+        objClass.classVariable = 7;
+
+        System.out.println("cls var = " + objClass.classVariable);
+
+        JavaTests.classVariable2 = 37;
+        System.out.println("cls var2 = " + JavaTests.classVariable2);
+
+        System.out.println("\n");
+
+        System.out.println(variableA);
+
+        System.out.println(objClass.variableB);
+
+        System.out.println(objClass.variableC);
+
+        System.out.println(objClass.variableD);
+
+        int varE = 12312312;
+        System.out.println(varE);
+
+        System.out.println("\n");
+
+
+        System.out.println(PI);
+
+        int w = 55555;
+
+        System.out.printf("The value of variable x is %d", w);
+
+        double zz = 12.1313467445;
+
+        System.out.printf("The value of variable x is %d%n " +
+                "The value of variable y is %.4f", w, zz);
+
+        System.out.println("\n");
+
+        int zx = 3;
+
+        int yx = zx;
+
+        System.out.println(yx);
+
+        System.out.println("\n");
+
+        int a = 10;
+
+        int b = a << 1;
+
+        System.out.println(a); // 10
+
+        System.out.println(b); // 20
+
+        // metoda toBinaryString()returnează reprezentaţia binară string a numărului întreg (în baza 2)
+
+        System.out.println(Integer.toBinaryString(a)); // 1010
+
+        System.out.println(Integer.toBinaryString(b)); // 10100
+
+        int aa = 10;
+
+        int ab = ~aa;
+
+        System.out.println(ab); // -11
+
+        System.out.println(Integer.toBinaryString(ab));
+        // 11111111111111111111111111110101
+        System.out.println(Integer.toBinaryString(5 & 11)); // 1 tj. 0001
+
+        System.out.println("\n");
+
+        int[] arrayx = new int[5];
+        arrayx[0] = 1314;
+        arrayx[3] = 5;
+        int[] array2 = {131, 312, 312, 321, 312};
+
+        Object objectArray[] = {3, 6.5, 2, 6, 1, 6};
+
+        System.out.println(arrayx.length);
+
+        System.out.println("\n");
+
+        int array[] = {3, 5, 2, 6, 1, 6};
+
+        int array1[] = {12, 13, 14};
+
+        int destArray[] = new int[array.length + array1.length];
+
+        System.arraycopy(array, 0, destArray, 0, array.length);
+
+        System.arraycopy(array1, 0, destArray, array.length, array1.length);
+
+        System.out.println("Array length is: " + destArray.length);
+
+        System.out.println("Elements are: ");
+
+        System.out.println(Arrays.toString(destArray));
+        System.out.println("\n");
+
+
+        int arr[] = {3, 5, 2, 6, 1, 6};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+        int[] a6 = {5, 1, 3, 4, 2};
+
+        Arrays.sort(a6);
+
+        System.out.println(Arrays.binarySearch(a6, 2));
+
+        System.out.println(Arrays.binarySearch(a6, 6));
+
+        System.out.println("\n");
+
+        int[] array3 = new int[6];
+
+        Arrays.fill(array, 7);
+        int[] array4 = new int[6];
+
+        System.out.println("\n");
+
+        int[][] matrix
+
+                = {
+                {1, 2, 3, 4},
+
+                {5, 6, 7, 8},
+
+                {9, 10, 11, 12}
+        };
+
+        System.out.println(matrix[1][2]);
+
+
 
     }
 }
